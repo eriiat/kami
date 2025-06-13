a83194343149ca78060fdfefbdc7daad9607ad19
eriiat <76994322+eriiat@users.noreply.github.com>
2025-06-14 01:22:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/license_system'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sjzeriiat:Aa123*845257@localhost/license_system'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True  # 调试时开启，生产环境关闭
